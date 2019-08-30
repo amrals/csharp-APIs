a0045e669c143894a8d8182567e4f878744c6db0
Matheus Amaral De Camargo Cardoso Lima <41813664803@info.local>
2019-08-30 11:13:55 -0300

    30/08

---
+++ Ekips/Senai.Ekips.WebApi/Senai.Ekips.WebApi/Repositories/UsuariosRepository.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Senai.Ekips.WebApi.Domains;
+using Senai.Ekips.WebApi.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Senai.Ekips.WebApi.Repositories
+{
+    public class UsuariosRepository
+    {
+        public Usuarios BuscarPorEmailESenha(LoginViewModel login)
+        {
+            using (EkipsContext ctx = new EkipsContext())
+            {
+                // buscar os dados no banco e verificar se este email e senha sao validos
+                Usuarios UsuarioBuscado = ctx.Usuarios.Include(x => x.IdPermissaoNavigation).FirstOrDefault(x => x.Email == login.Email && x.Senha == login.Senha);
+                if (UsuarioBuscado == null)
+                {
+                    return null;
+                }
+                return UsuarioBuscado;
+            }
+        }
+    }
+}
